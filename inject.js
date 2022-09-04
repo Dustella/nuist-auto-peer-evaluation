6aa12a95751d15037f92e574985d9795bd5692f3
Dustella <fdnoaivj@outlook.com>
2022-09-05 04:28:52 +0800

    🐛 last col lowers 1

--- inject.js
+++ inject.js
@@ -14,7 +14,6 @@
 // @homepageURL  https://github.com/XIU2/UserScript
 // ==/UserScript==
 
-
 (() => {
   const getLabelElements = () =>
     document
@@ -28,19 +27,22 @@
 
   const fillValue = (form, genValue) => {
     for (const row of form.children) {
+      let cnt = 0;
       for (const { firstElementChild: item } of row.children) {
         try {
-          item.value = genValue();
+          item.value = ++cnt === 8 ? genValue() - 1 : genValue();
         } catch {}
       }
     }
   };
 
   const toggleHelper = () => {
-    document.querySelector("#r_3_3").insertAdjacentHTML(
-      "beforebegin",
-      `<div class="toggle-helper">生成随机数并填写<input type="text" class="innum" /><input type="text" class="innum" /><button id='laing'>一键填写</button></div>`
-    );
+    document
+      .querySelector("#r_3_3")
+      .insertAdjacentHTML(
+        "beforebegin",
+        `<div class="toggle-helper">生成随机数并填写<input type="text" class="innum" /><input type="text" class="innum" /><button id='laing'>一键填写</button></div>`
+      );
     document.querySelector("#laing").addEventListener("click", (e) => {
       e.preventDefault();
       e.stopPropagation();
